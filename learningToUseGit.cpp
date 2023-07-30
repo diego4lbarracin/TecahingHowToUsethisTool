f3f88e3edc4e0a8b0c396237c3558e63cea7b490
DiegoAlbarracin <diegoalbarracin0405@gmail.com>
2023-07-30 15:37:00 -0500

    Adding functionalities in the brach 'Diego Dev1'

--- learningToUseGit.cpp
+++ learningToUseGit.cpp
@@ -4,10 +4,14 @@ using namespace std;
 int main(){
     cout << "I'm learning to use git :D" << endl;
     
+    int arra[10];
     for (int i = 0; i < 10; i++)
     {
         cout << "Number: " << i << endl;
+        arra[i] = i;
     }
+    
+    cout << "I'm in a different branch" << endl;
     
     
     return 0;
