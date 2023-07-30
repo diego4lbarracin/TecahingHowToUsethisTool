194c1ea66a4b769e26e75550e2350c0ff3f97e21
DiegoAlbarracin <diegoalbarracin0405@gmail.com>
2023-07-30 15:27:08 -0500

    Adding a for loop

--- learningToUseGit.cpp
+++ learningToUseGit.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main(){
     cout << "I'm learning to use git :D" << endl;
+    
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Number: " << i << endl;
+    }
+    
+    
     return 0;
 }
